7fd6a988d722a9ae42f9f79f7a4ae4750c2e1111
matheuefranco <matheusefranco@gmail.com>
2025-11-05 09:10:00 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class App {
+
+
+    static void menu() {
+        System.out.println("1. Adicionar Vértice");
+        System.out.println("2. Adicionar Aresta");
+        System.out.println("3. Mostrar Grafo");
+        System.out.println("4. Verificar Alcance");
+        System.out.println("0. Sair");
+        System.out.print("Escolha uma opção: ");
+    }
+
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        HashMap<Integer, Cidade> cepCidades = new HashMap<>();
+
+    }
+}
